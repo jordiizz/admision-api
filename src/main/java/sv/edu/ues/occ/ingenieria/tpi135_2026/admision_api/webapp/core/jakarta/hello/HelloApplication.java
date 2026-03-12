40126f9cfbbe55ad49358072c20ee238d5755878
Jordi Martínez <michaeljordimartinez@gmail.com>
2026-03-11 19:35:43 -0600

    Modificado: archivos mwn removidos

--- src/main/java/sv/edu/ues/occ/ingenieria/tpi135_2026/admision_api/webapp/core/jakarta/hello/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package sv.edu.ues.occ.ingenieria.tpi135_2026.admision_api.webapp.core.jakarta.hello;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("rest")
-public class HelloApplication extends Application {
-    // Needed to enable Jakarta REST and specify path.
-}
