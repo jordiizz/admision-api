fb15f9569bbb501c75db4da319c8ba633fb37a89
Ever Granadino <gm23004@ues.edu.sv>
2026-03-14 21:01:22 -0600

    Agregado: EndPoints CRUD para DistractorResource y buscarPorId

---
+++ src/main/java/sv/edu/ues/occ/ingenieria/tpi135_2026/admision_api/webapp/core/boundary/rest/DistractorResource.java
@@ -0,0 +1,130 @@
+package sv.edu.ues.occ.ingenieria.tpi135_2026.admision_api.webapp.core.boundary.rest;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriBuilder;
+import jakarta.ws.rs.core.UriInfo;
+import sv.edu.ues.occ.ingenieria.tpi135_2026.admision_api.webapp.core.control.DistractorDAO;
+import sv.edu.ues.occ.ingenieria.tpi135_2026.admision_api.webapp.core.entity.Distractor;
+
+@Path("distractor")
+public class DistractorResource implements Serializable {
+
+    @Inject
+    DistractorDAO distractorDAO;
+
+    @POST
+    @Produces({MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
+    public Response crear(Distractor distractor, @Context UriInfo uriInfo) {
+        if (distractor != null && distractor.getIdDistractor() == null) {
+            try {
+                distractor.setIdDistractor(UUID.randomUUID());
+                distractorDAO.crear(distractor);
+                UriBuilder uriBuilder = uriInfo.getAbsolutePathBuilder();
+                uriBuilder.path(distractor.getIdDistractor().toString());
+                return Response.created(uriBuilder.build()).entity(distractor).build();
+            } catch (Exception e) {
+                return Response.status(500).header(ResponseHeaders.PROCESS_ERROR.toString(), e.getMessage()).build();
+            }
+        }
+        return Response.status(422).header(ResponseHeaders.WRONG_PARAMETER.toString(), "El recurso no puede ser nulo y no debe tener un ID asignado").build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Response eliminar(@PathParam("id") UUID id) {
+        if (id != null) {
+            try {
+                Distractor distractor = distractorDAO.buscarPorId(id);
+                if (distractor != null) {
+                    distractorDAO.eliminar(distractor);
+                    return Response.noContent().build();
+                }
+                return Response.status(404).header(ResponseHeaders.NOT_FOUND.toString(), "Recurso no encontrado").build();
+            } catch (Exception e) {
+                return Response.status(500).header(ResponseHeaders.PROCESS_ERROR.toString(), e.getMessage()).build();
+            }
+        }
+        return Response.status(422).header(ResponseHeaders.WRONG_PARAMETER.toString(), "El ID no puede ser nulo").build();
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response buscarPorRango(
+            @QueryParam("first")
+            @DefaultValue("0")
+            int firstResult,
+            @QueryParam("max")
+            @DefaultValue("50")
+            int maxResults) {
+        try {
+            if (firstResult >= 0 && maxResults > 0 && maxResults <= 50) {
+                List<Distractor> distractores = distractorDAO.buscarPorRango(firstResult, maxResults);
+                Long total = distractorDAO.contar();
+                Response.ResponseBuilder responseBuilder = Response.ok(distractores)
+                        .header(ResponseHeaders.TOTAL_RECORDS.toString(), total)
+                        .type(MediaType.APPLICATION_JSON);
+                return responseBuilder.build();
+            } else {
+                return Response.status(422).header(ResponseHeaders.WRONG_PARAMETER.toString(), "Los parámetros 'first' debe ser >= 0 y 'max' debe ser > 0 y <= 50").build();
+            }
+        } catch (Exception e) {
+            return Response.status(500).header(ResponseHeaders.PROCESS_ERROR.toString(), e.getMessage()).build();
+        }
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response buscarPorId(@PathParam("id") UUID id) {
+        if (id != null) {
+            try {
+                Distractor encontrado = distractorDAO.buscarPorId(id);
+                if (encontrado != null) {
+                    Response.ResponseBuilder builder = Response.ok(encontrado).type(MediaType.APPLICATION_JSON);
+                    return builder.build();
+                }
+                return Response.status(404).header(ResponseHeaders.NOT_FOUND.toString(), "Recurso no encontrado").build();
+            } catch (Exception e) {
+                return Response.status(500).header(ResponseHeaders.PROCESS_ERROR.toString(), e.getMessage()).build();
+            }
+        }
+        return Response.status(422).header(ResponseHeaders.WRONG_PARAMETER.toString(), "id: " + id).build();
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response actualizar(Distractor distractor) {
+        if (distractor != null && distractor.getIdDistractor() != null) {
+            try {
+                Distractor existente = distractorDAO.buscarPorId(distractor.getIdDistractor());
+                if (existente != null) {
+                    distractorDAO.actualizar(distractor);
+                    return Response.ok(distractor).build();
+                }
+                return Response.status(404).header(ResponseHeaders.NOT_FOUND.toString(), "Recurso no encontrado").build();
+            } catch (Exception e) {
+                return Response.status(500).header(ResponseHeaders.PROCESS_ERROR.toString(), e.getMessage()).build();
+            }
+        }
+        return Response.status(422).header(ResponseHeaders.WRONG_PARAMETER.toString(), "El recurso no puede ser nulo y debe tener un ID asignado").build();
+    }
+}
