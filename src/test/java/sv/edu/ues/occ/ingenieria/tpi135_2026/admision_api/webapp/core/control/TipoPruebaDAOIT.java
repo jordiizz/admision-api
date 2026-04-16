d60e65809aed7dc470a7dffde97fc96f9ce237d8
Jordi Martínez <michaeljordimartinez@gmail.com>
2026-04-15 19:31:47 -0600

    add: pruebas de integración faltantes de para TipoPruebaDAO

--- src/test/java/sv/edu/ues/occ/ingenieria/tpi135_2026/admision_api/webapp/core/control/TipoPruebaDAOIT.java
+++ src/test/java/sv/edu/ues/occ/ingenieria/tpi135_2026/admision_api/webapp/core/control/TipoPruebaDAOIT.java
@@ -1,8 +1,5 @@
 package sv.edu.ues.occ.ingenieria.tpi135_2026.admision_api.webapp.core.control;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import java.util.List;
 import java.util.UUID;
 
@@ -15,10 +12,12 @@ import org.junit.jupiter.api.TestMethodOrder;
 
 import sv.edu.ues.occ.ingenieria.tpi135_2026.admision_api.webapp.core.entity.TipoPrueba;
 
+import static org.junit.Assert.*;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TipoPruebaDAOIT extends AbstractIntengrationDAOTest {
-   
+
     TipoPruebaDAO cut;
     TipoPrueba tipoPrueba = new TipoPrueba(UUID.randomUUID());
     TipoPrueba tipoPrueba2 = new TipoPrueba(UUID.randomUUID());
@@ -59,7 +58,29 @@ public class TipoPruebaDAOIT extends AbstractIntengrationDAOTest {
 
     @Order(3)
     @Test
+    public void testCrearTipoPruebaEmNull(){
+        cut.em = null;
+        try{
+            cut.crear(tipoPrueba2);
+        } catch (Exception ex) {
+            assertEquals(IllegalStateException.class, ex.getClass());
+        }
+    }
+
+    @Order(4)
+    @Test
+    public void testCrearTipoPruebaParametrosInvalidos(){
+        try{
+            cut.crear(null);
+        } catch (Exception ex) {
+            assertEquals(IllegalArgumentException.class, ex.getClass());
+        }
+    }
+
+    @Order(5)
+    @Test
     public void testActualizarTipoPruebaExitoso(){
+        cut.em = em;
         cantidadRegistros = cut.contar();
         tipoPrueba.setValor("ACTUALIZADO");
         TipoPrueba tipoPruebaActualizado = cut.actualizar(tipoPrueba);
@@ -67,35 +88,155 @@ public class TipoPruebaDAOIT extends AbstractIntengrationDAOTest {
         assertTrue(cantidadRegistrosDespues.equals(cantidadRegistros));
     }
 
-    @Order(4)
+    @Order(6)
+    @Test
+    public void testActualizarTipoPruebaEmNull(){
+        cut.em = null;
+        try{
+            cut.actualizar(tipoPrueba);
+        }catch (Exception ex){
+            assertEquals(IllegalStateException.class, ex.getClass());
+        }
+    }
+
+    @Order(7)
+    @Test
+    public void testActualizarTipoPruebaParametroNull(){
+        cut.em = em;
+        try{
+            cut.actualizar(null);
+        }catch (Exception ex){
+            assertEquals(IllegalArgumentException.class, ex.getClass());
+        }
+    }
+
+    @Order(8)
     @Test
     public void testBuscarTipoPruebaPorIdExitoso(){
         TipoPrueba tipoPruebaEncontrado = cut.buscarPorId(tipoPrueba.getIdTipoPrueba());
-        assertEquals(tipoPrueba, tipoPruebaEncontrado); 
+        assertEquals(tipoPrueba, tipoPruebaEncontrado);
     }
 
-    @Order(5)
+    @Order(9)
+    @Test
+    public void testBuscarTipoPruebaPorIdEmNull(){
+        cut.em = null;
+        try{
+            cut.buscarPorId(tipoPrueba.getIdTipoPrueba());
+        }catch (Exception ex){
+            assertEquals(IllegalStateException.class, ex.getClass());
+        }
+    }
+
+    @Order(10)
+    @Test
+    public void testBuscarTipoPruebaPorIdParametroNull(){
+        cut.em = em;
+        try{
+            cut.buscarPorId(null);
+        }catch (Exception ex){
+            assertEquals(IllegalArgumentException.class, ex.getClass());
+        }
+    }
+
+    @Order(11)
     @Test
     public void testBuscarTipoPruebaPorRangoExitoso(){
+        cut.em = em;
         em.getTransaction().begin();
         cut.crear(tipoPrueba2);
         cut.crear(tipoPrueba3);
         List<TipoPrueba> tipoPruebas = cut.buscarPorRango(0, 3);
         em.getTransaction().commit();
         assertTrue(tipoPruebas.size() == 3);
-    }   
+    }
 
-    @Order(6)
+    @Order(12)
+    @Test
+    public void testBuscarTipoPruebaPorRangoEmNull(){
+        cut.em = null;
+        try{
+            cut.buscarPorRango(0, 10);
+        }catch (Exception ex){
+            assertEquals(IllegalStateException.class, ex.getClass());
+        }
+    }
+
+    @Order(13)
+    @Test
+    public void testBuscarTipoPruebaPorRangoFirstNegativo(){
+        cut.em = em;
+        try{
+            cut.buscarPorRango(-1, 10);
+        }catch (Exception ex){
+            assertEquals(IllegalArgumentException.class, ex.getClass());
+        }
+    }
+
+    @Order(14)
+    @Test
+    public void testBuscarTipoPruebaPorRangoMaxNegativo(){
+        cut.em = em;
+        try{
+            cut.buscarPorRango(0, -1);
+        }catch (Exception ex){
+            assertEquals(IllegalArgumentException.class, ex.getClass());
+        }
+    }
+
+    @Order(15)
     @Test
     public void testEliminarTipoPruebaExitoso(){
+        cut.em = em;
         em.getTransaction().begin();
         cantidadRegistros = cut.contar();
         cut.eliminar(tipoPrueba);
         cut.eliminar(tipoPrueba2);
-        cut.eliminar(tipoPrueba3);
         cantidadRegistrosDespues = cut.contar();
         em.getTransaction().commit();
         assertTrue(cantidadRegistrosDespues < cantidadRegistros);
+    }
+
+    @Order(16)
+    @Test
+    public void testEliminarTipoPruebaEmNull(){
+        cut.em = null;
+        try{
+            cut.eliminar(tipoPrueba);
+        }catch (Exception ex){
+            assertEquals(IllegalStateException.class, ex.getClass());
+        }
+    }
+
+    @Order(17)
+    @Test
+    public void testEliminarTipoPruebaParametroNull(){
+        try{
+            cut.eliminar(null);
+        }catch (Exception ex){
+            assertEquals(IllegalArgumentException.class, ex.getClass());
+        }
+    }
+
+    @Order(18)
+    @Test
+    public void testContarTipoPruebaEmNull(){
+        cut.em = null;
+        try{
+            cut.contar();
+        }catch (Exception ex){
+            assertEquals(IllegalStateException.class, ex.getClass());
+        }
+    }
+
+    @Order(19)
+    @Test
+    public void testEliminarNoContained(){
+        em.clear();
+        cut.eliminar(tipoPrueba3);
+        TipoPrueba encontrado = cut.buscarPorId(tipoPrueba3.getIdTipoPrueba());
+
+        assertNull(encontrado);
     }
 
 }
