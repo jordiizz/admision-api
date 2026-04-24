7d400a8784942746028258e1cd3051b514d3ee16
E-AlexGM <eagranadinomendoza@gmail.com>
2026-04-23 22:05:16 -0600

    Add: Agregados todos los cors para poder acceder desde el FrontEnd

--- src/main/java/sv/edu/ues/occ/ingenieria/tpi135_2026/admision_api/webapp/core/boundary/rest/CorseResponseFilter.java
+++ src/main/java/sv/edu/ues/occ/ingenieria/tpi135_2026/admision_api/webapp/core/boundary/rest/CorseResponseFilter.java
@@ -16,31 +16,38 @@ public class CorseResponseFilter implements ContainerResponseFilter {
     public static final String METODOS_PERMITIDOS="GET, POST, PUT, DELETE, OPTIONS, HEAD";
     public static final int MAXIMO_CACHE=30*60*60;
     public static final String CABECERAS_PERMITIDAS="origin,accept,content-type";
-    public static final String CABECERAS_EXPUESTAS="location,info";
+    public static final String CABECERAS_EXPUESTAS="location,info,Total-Records";
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
         MultivaluedMap<String, Object> headers = responseContext.getHeaders();
         headers.add("Access-Control-Allow-Origin", "*");
-        headers.add("Access-Control-Allow-Headers", getRequestAllowHeaders(responseContext));
-        headers.add("Access-Control-Expose-Headers", CABECERAS_PERMITIDAS);
+        headers.add("Access-Control-Allow-Headers", getRequestedAllowedHeaders(requestContext));
+        headers.add("Access-Control-Expose-Headers", getRequestedExposedHeaders(requestContext));
+        headers.add("Access-Control-Allow-Credentials", "true");
+        headers.add("Access-Control-Allow-Methods", METODOS_PERMITIDOS);
+        headers.add("Access-Control-Max-Age", MAXIMO_CACHE);
+        headers.add("x-responded-by", "cors-response-filter");
     }
 
-    String getRequestAllowHeaders(ContainerResponseContext responseContext) {
-        List<Object> headers = responseContext.getHeaders().get("Access-Control-Allow-Headers");
+    String getRequestedAllowedHeaders(ContainerRequestContext responseContext) {
+        List<String> headers = responseContext.getHeaders().get("Access-Control-Allow-Headers");
         return crearCabeceras(headers, CABECERAS_PERMITIDAS);
     }
 
-    String crearCabeceras(List<Object> cabeceras, String cabecerasPorDefecto){
+    String getRequestedExposedHeaders(ContainerRequestContext requestContext) {
+        List<String> headers = requestContext.getHeaders().get("Access-Control-Expose-Headers");
+        return crearCabeceras(headers, CABECERAS_EXPUESTAS);
+    }
 
-        List<String> salidas = new ArrayList<>();
-
+    String crearCabeceras(List<String> cabeceras, String cabecerasPorDefecto){
         if (cabeceras==null || cabeceras.isEmpty()){
             return cabecerasPorDefecto;
         }
+        List<String> salida = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
         for (Object cabecera : cabeceras){
-            sb.append(cabecera.toString());
+            sb.append(cabecera);
             sb.append(";");
         }
         sb.append(cabecerasPorDefecto);
